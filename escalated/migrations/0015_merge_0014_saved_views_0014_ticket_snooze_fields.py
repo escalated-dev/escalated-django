b766a637482076d848fb6f797327a3d2819944d5
Matt Gros <3311227+mpge@users.noreply.github.com>
2026-04-08 07:19:58 -0400

    fix: merge conflicting migrations
    Adds merge migration for 0014_saved_views and 0014_ticket_snooze_fields

---
+++ escalated/migrations/0015_merge_0014_saved_views_0014_ticket_snooze_fields.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("escalated", "0014_saved_views"),
+        ("escalated", "0014_ticket_snooze_fields"),
+    ]
+
+    operations = []
